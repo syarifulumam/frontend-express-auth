e9d6a27f236116a47779bf86b95039cece35fde0
syarifulumam <syarifulumam7@gmail.com>
2022-09-06 22:50:21 +0700

    Add : menambah redux presist

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import authReducer from '../features/authSlice'
 import userReducer from '../features/userSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducer = combineReducers({
+  auth:authReducer,
+  user:userReducer
+})
+
+const persistedReducer = persistReducer(persistConfig,reducer)
 
 export const store = configureStore({
-  reducer: {
-    auth:authReducer,
-    user:userReducer
-  },
+  reducer: persistedReducer
 })
